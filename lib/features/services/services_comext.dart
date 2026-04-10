a5233749bfb3a8e17e73716bba7b61c0e6039921
JoseIglesias <joseiglesiasrep@gmail.com>
2026-04-09 20:24:16 -0500

    Primer commit: Proyecto OCEANCOMEX Flutter Web

--- lib/features/services/services_comext.dart
+++ lib/features/services/services_comext.dart
@@ -61,7 +61,7 @@ class ServicesComext extends StatelessWidget {
                   boxShadow: [
                     BoxShadow(
                       blurRadius: 20,
-                      color: Colors.grey.withOpacity(0.1),
+                   color: Colors.grey.withValues(alpha: 0.1),
                       offset: const Offset(0, 10),
                     )
                   ],
